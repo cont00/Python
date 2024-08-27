a6f37e3cc5c015eddaef66891504fa5173616497
cont00 <38021605+cont00@users.noreply.github.com>
2024-08-27 15:53:13 +0900

    Update student.py

--- student.py
+++ student.py
@@ -4,7 +4,7 @@ import pickle
 def cls():
     os.system('cls')
 
-def insert(student):
+def Insert(student):
     cls()
     name = input("학생 이름 입력: ")
     kor = int(input("국어 성적 입력: "))
@@ -16,7 +16,7 @@ def insert(student):
     person = student[name]
     return student
 
-def view(student):
+def View(student):
     cls()
     print(f'{"=":=^48}')
     print(f'{"이름":<12} {"국어":>4} {"영어":>4} {"수학":>4} {"성적":>6}')
@@ -25,105 +25,105 @@ def view(student):
     print(f'{"=":=^48}')
     return student
 
-def search(student):
+def Search(student):
     cls()
-    search_name = input("찾는 대상의 이름 입력: ")
-    if (search_name in student) == True:
+    search_Name = input("찾는 대상의 이름 입력: ")
+    if (search_Name in student) == True:
         print(f'{"=":=^48}')
         print(f'{"이름":<12} {"국어":>4} {"영어":>4} {"수학":>4} {"성적":>4}')
-        print(f'{search_name:<12}', f'{student[search_name][0]:>8}', f'{student[search_name][1]:>6}', f'{student[search_name][2]:>6}', f'{student[search_name][3]:>8}')
+        print(f'{search_Name:<12}', f'{student[search_Name][0]:>8}', f'{student[search_Name][1]:>6}', f'{student[search_Name][2]:>6}', f'{student[search_Name][3]:>8}')
         print(f'{"=":=^48}')
     else:
         print("찾는 대상이 존재하지 않습니다.")
         print("1. 재검색    2. 되돌아가기")
-        search_select = int(input("번호입력: "))
-        if search_select == 1:
-            search(student)
-        elif search_select == 2:
-            main()
+        search_Select = int(input("번호입력: "))
+        if search_Select == 1:
+            Search(student)
+        elif search_Select == 2:
+            Main()
         else:
             print("1 혹은 2 를 입력해주세요.")
             print("그 외 다른 번호입력은 오류를 에러를 발생시킵니다.")
             print("1. 재검색    2. 되돌아가기")
-            search_select = int(input("번호입력: "))
-            if search_select == 1:
-                search(student)
-            elif search_select == 2:
-                main()
+            search_Select = int(input("번호입력: "))
+            if search_Select == 1:
+                Search(student)
+            elif search_Select == 2:
+                Main()
 
     return student
 
-def updata(student):
+def UpData(student):
     cls()
-    updata_name = input("수정 대상의 이름 입력: ")
-    if (updata_name in student) == True:
+    updata_Name = input("수정 대상의 이름 입력: ")
+    if (updata_Name in student) == True:
         print("학생의 이름도 수정하시겠습니까? 1. Yes   2. No")
         num = int(input("번호입력: "))
         if num == 1:
-            student.pop(updata_name)
-            updata_name = input("학생 이름 입력: ")
+            student.pop(updata_Name)
+            updata_Name = input("학생 이름 입력: ")
             kor = int(input("국어 성적 입력: "))
             eng = int(input("영어 성적 입력: "))
             math = int(input("수학 성적 입력: "))
             result = kor + eng + math
             avg = round((result / 3), 2)
-            student[updata_name] = [kor, eng, math, avg]
-            person = student[updata_name]
+            student[updata_Name] = [kor, eng, math, avg]
+            person = student[updata_Name]
         elif num == 2:
             kor = int(input("국어 성적 입력: "))
             eng = int(input("영어 성적 입력: "))
             math = int(input("수학 성적 입력: "))
             result = kor + eng + math
             avg = round((result / 3), 2)
-            student[updata_name] = [kor, eng, math, avg]
-            person = student[updata_name]
+            student[updata_Name] = [kor, eng, math, avg]
+            person = student[updata_Name]
 
     else:
         print("수정 대상이 존재하지 않습니다.")
         print("1. 재검색    2. 메인으로 되돌아가기")
-        updata_select = input("번호입력: ")
-        if updata_select == 1 :
-            updata(student)
-        elif updata_select == 2:
-            main()
+        updata_Select = input("번호입력: ")
+        if updata_Select == 1 :
+            UpData(student)
+        elif updata_Select == 2:
+            Main()
         else:
             print("1 혹은 2 를 입력해주세요.")
             print("그 외 다른 번호입력은 오류를 에러를 발생시킵니다.")
             print("1. 재검색    2. 메인으로 되돌아가기")
-            updata_select = int(input("번호입력: "))
-            if updata_select == 1 :
-                updata(student)
-            elif updata_select == 2:
-                main()
+            updata_Select = int(input("번호입력: "))
+            if updata_Select == 1 :
+                UpData(student)
+            elif updata_Select == 2:
+                Main()
     return student
 
-def delete(student):
+def Delete(student):
     cls()
-    delete_name = input("데이터를 삭제할 학생의 이름을 입력: ")
-    if (delete_name in student) == True:
+    delete_Name = input("데이터를 삭제할 학생의 이름을 입력: ")
+    if (delete_Name in student) == True:
         print("해당 학생의 데이터를 삭제하시겠습니까? 1. Yes    2. No")
-        delete_select = int(input("번호 입력: "))
-        if delete_select == 1:
-            student.pop(delete_name)
-        elif delete_select == 2:
+        delete_Select = int(input("번호 입력: "))
+        if delete_Select == 1:
+            student.pop(delete_Name)
+        elif delete_Select == 2:
             print("해당 학생의 데이터 삭제를 취소하였습니다.")
-            delete(student)
+            Delete(student)
     return student
 
-def alldelete(student):
+def AllDelete(student):
     cls()
     print("입력되어있는 모든 학생의 데이터를 삭제하시겠습니까? 1. Yes   2. No")
-    alldelete_select = int(input("번호입력: "))
-    if alldelete_select == 1:
+    alldelete_Select = int(input("번호입력: "))
+    if alldelete_Select == 1:
         student.clear()
-    elif alldelete_select == 2:
-        main()
+    elif alldelete_Select == 2:
+        Main()
     else:
         print("1 혹은 2를 입력해주세요")
-        alldelete()
+        AllDelete()
     return student
 
-def fileupload(student):
+def FileUpLoad(student):
     cls()
     f = open("D:\김현노\Python\ex\student.txt", 'r', encoding='UTF-8')
     lines = f.readlines()
@@ -135,7 +135,7 @@ def fileupload(student):
         student = pickle.load(fr)
     return student
 
-def fileupdata(student):
+def FileUpData(student):
     cls()
     with open("D:\김현노\Python\ex\student.txt", 'w', encoding='UTF-8') as f:
         f.write("================================================\n")
@@ -147,7 +147,7 @@ def fileupdata(student):
         pickle.dump(student, fw)
     return student
 
-def main():
+def Main():
     cls()
     student = dict()
     while True:
@@ -156,23 +156,23 @@ def main():
         select = int(input())
         
         if select == 1:
-            student = insert(student)
+            student = Insert(student)
         elif select == 2:
-            student = view(student)
+            student = View(student)
         elif select == 3:
-            student = search(student)
+            student = Search(student)
         elif select == 4:
-            student = updata(student)
+            student = UpData(student)
         elif select == 5:
-            student = delete(student)
+            student = Delete(student)
         elif select == 6:
-            student = alldelete(student)
+            student = AllDelete(student)
         elif select == 7:
-            student = fileupload(student)
+            student = FileUpLoad(student)
         elif select == 8:
-            student = fileupdata(student)
+            student = FileUpData(student)
         else:
             print("프로그램을 종료합니다.")
             break
 
-main()
+Main()
